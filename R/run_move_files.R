c51aab264e56b7b3365fca407e02c7af0c761095
Marcos Alves <mppalves@gmail.com>
2021-03-02 15:46:51 +0100

    adding mean and stdv to move script

--- R/run_move_files.R
+++ R/run_move_files.R
@@ -10,7 +10,7 @@ run_move_files <- function(source_folder, modelid, targetdir) {
   hash <- substr(modelid, 1, 6)
   output_dir <- grep(modelid, list.dirs(targetdir, recursive = F), value = T)
   setwd(output_dir)
-  files <- grep(paste0("^", hash), list.files(), value = T, perl = T)
+  files <- grep(paste0("^", hash, "|mean_",hash, "|stddevs_",hash), list.files(), value = T, perl = T)
   dpath <- file.path(source_folder, hash)
   dir.create(dpath, showWarnings = F)
   file.copy(files, to = dpath, overwrite = T)

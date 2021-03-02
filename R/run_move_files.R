93e063a159623e88ac936071278f8b5b092b6552
Marcos Alves <mppalves@gmail.com>
2021-03-02 16:25:02 +0100

    feature files move

--- R/run_move_files.R
+++ R/run_move_files.R
@@ -10,17 +10,20 @@ run_move_files <- function(source_folder, modelid, targetdir) {
   hash <- substr(modelid, 1, 6)
   output_dir <- grep(modelid, list.dirs(targetdir, recursive = F), value = T)
   setwd(output_dir)
-  files <- grep(paste0("^", hash), list.files(), value = T, perl = T)
+  files1 <- grep(paste0("^", hash), list.files(), value = T, perl = T)
   dpath <- file.path(source_folder, hash)
   dir.create(dpath, showWarnings = F)
-  file.copy(files, to = dpath, overwrite = T)
+  file.copy(files1, to = dpath, overwrite = T)
   dirs <- list.dirs()
   last_dir <- dirs[length(dirs)]
   if (grepl("[0-9]{4}", last_dir)) {
     setwd(last_dir)
-    files <- grep(paste0("mean+|stddevs+"), list.files(), value = T, perl = T)
-    file.copy(files, to = dpath, overwrite = T)
+    files2 <- grep(paste0("mean+|stddevs+"), list.files(), value = T, perl = T)
+    file.copy(files2, to = dpath, overwrite = T)
   } else {
     stop("Means and stds could not be copied")
   }
+  print(paste0("Files moved to ", source_folder))
+  print(paste(files1))
+  print(paste(files2))
 }

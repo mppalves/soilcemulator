5830f4061cffb62786ae6e14b7aad7ebd30cc92e
Marcos Alves <mppalves@gmail.com>
2021-03-02 16:10:32 +0100

    copying means and stddvs

--- R/run_move_files.R
+++ R/run_move_files.R
@@ -10,8 +10,17 @@ run_move_files <- function(source_folder, modelid, targetdir) {
   hash <- substr(modelid, 1, 6)
   output_dir <- grep(modelid, list.dirs(targetdir, recursive = F), value = T)
   setwd(output_dir)
-  files <- grep(paste0("^", hash, "|mean_",hash, "|stddevs_",hash), list.files(), value = T, perl = T)
+  files <- grep(paste0("^", hash), list.files(), value = T, perl = T)
   dpath <- file.path(source_folder, hash)
   dir.create(dpath, showWarnings = F)
   file.copy(files, to = dpath, overwrite = T)
+  dirs <- list.dirs()
+  last_dir <- dirs[length(dirs)]
+  if (grepl("[0-9]{4}", last_dir)) {
+    setwd(last_dir)
+    files <- grep(paste0("mean+|stddevs+"), list.files(), value = T, perl = T)
+    file.copy(files, to = dpath, overwrite = T)
+  } else {
+    stop("Means and stds could not be copied")
+  }
 }

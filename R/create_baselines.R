2cf8bdd09ea13b35a912ae5a96519a897cf24744
Marcos Alves <mppalves@gmail.com>
2021-08-12 15:41:22 +0200

    bug fix

--- R/create_baselines.R
+++ R/create_baselines.R
@@ -8,26 +8,33 @@
 #' @export
 
 create_baselines <- function(modelid, targetdir, pprosdf) {
-
   output_dir <- grep(modelid, list.dirs(targetdir, recursive = F), value = T)
   setwd(output_dir)
-  files <- grep("output", list.files(), value = T, perl = T)
-  y_hat <- readRDS(grep(".rds", files, value = T, ignore.case = FALSE))
 
-  files <- grep("stddevs_lab", list.files(), value = T, perl = T)
-  std <- readRDS(grep(".rds", files, value = T, ignore.case = FALSE))
+  dirs <- list.dirs()
+  last_dir <- dirs[length(dirs)]
+  if (grepl("[0-9]{4}", last_dir)) {
+    setwd(last_dir)
+    files <- grep("output", list.files(), value = T, perl = T)
+    y_hat <- readRDS(grep(".rds", files, value = T, ignore.case = T))
 
-  files <- grep("mean_lab", list.files(), value = T, perl = T)
-  mean <- readRDS(grep(".rds", files, value = T, ignore.case = FALSE))
+    files <- grep("stddevs_lab", list.files(), value = T, perl = T)
+    std <- readRDS(grep(".rds", files, value = T, ignore.case = T))
 
-  y_hat <- y_hat * std + mean
+    files <- grep("mean_lab", list.files(), value = T, perl = T)
+    mean <- readRDS(grep(".rds", files, value = T, ignore.case = T))
 
-  lsus_name <- grep("lsu", colnames(pprosdf), value = T)
-  cells <- as.numeric(unique(pprosdf[["Cell"]]))
-  years <- unique(pprosdf[["Year"]])
-  lsus <- unique(pprosdf[[lsus_name]])
-  y_array <- array(y_hat, dim = c(59199, length(lsus), length(years)), dimnames = list(cells,lsus, years))
-  y_mag <- as.magpie(y_array, spatial = 1)
-  write.magpie(y_mag, paste0("baselines_",modelid, ".mz"))
-  return(y_mag)
+    y_hat <- y_hat * std + mean
+
+    lsus_name <- grep("lsu", colnames(pprosdf), value = T)
+    cells <- as.numeric(unique(pprosdf[["Cell"]]))
+    years <- unique(pprosdf[["Year"]])
+    lsus <- unique(pprosdf[[lsus_name]])
+    y_array <- array(y_hat, dim = c(59199, length(lsus), length(years)), dimnames = list(cells, lsus, years))
+    y_mag <- as.magpie(y_array, spatial = 1)
+    write.magpie(y_mag, paste0("baselines_", modelid, ".mz"))
+    return(y_mag)
+  } else {
+    stop("Error opeining files")
+  }
 }
